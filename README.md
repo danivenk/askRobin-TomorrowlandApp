d22c9b37975fb41c983238e07bbe67387924399a
danivenk <danivenk@gmail.com>
2025-02-03 23:18:37 +0100

    cleaned up some code, added dataset & added chat input on the page.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # askRobin-TomorrowlandApp
-Django App for technical assessment of askRobin
+Django App for technical assessment of askRobin it.
+The Tomorrowland data is retrieved from the [festival viewer](https://festivalviewer.com/tomorrowland/lineup/2024) website
--- tomorrowApp/tomorrow/views.py
+++ tomorrowApp/tomorrow/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.urls import get_resolver
 
+# save the messages of the chat
+messages = []
+
+
 def index(request):
-    print(get_resolver().url_patterns)
+    """
+    show the chat with input view
+    """
+
+    if request.method == "POST":
+        query = request.POST.get("query", None)
+        try:
+            new_chat = int(request.POST.get("new", 0))
+        except ValueError:
+            new_chat = 0
+        if query is not None:
+            messages.append({"role": "user", "content": query})
+        if new_chat:
+            messages.clear()
+        return redirect("index")
+    # print(get_resolver().url_patterns)
     # template = loader.get_template("tomorrow/index.html")
-    return render(request, "tomorrow/index.html")
+    context = {"messages": reversed(messages)}
+    print(context)
+    return render(request, "tomorrow/index.html", context)
